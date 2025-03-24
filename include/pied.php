78a9c076eca7a826d46f00ded1c0b7cadb123fa5
FarisMsr <mansourifaris52@gmail.com>
2025-03-24 09:37:38 +0100

    pied

--- include/pied.php
+++ include/pied.php
@@ -3,9 +3,6 @@
 <footer>
     <div class="footer-content">
         <p>&copy; 2025 Mansouri. Tous droits réservés.</p>
-        <p>
-            <a href="https://github.com/votreprofil">GitHub</a>
-        </p>
     </div>
 </footer>
 </body>
